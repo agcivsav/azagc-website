3df9ae3259c660f70621baf05f0e225ae90ef405
Muhammad Waqas <mwaqassabirofficial@gmail.com>
2026-06-02 19:16:36 +0500

    fixed redirect

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://www.azagc.org/', // change this
+  siteUrl: 'https://azagc.org/', // change this
   generateRobotsTxt: true,
   sitemapSize: 7000,
 };
